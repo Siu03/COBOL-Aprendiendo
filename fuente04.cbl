ee58bac64299ad5fbb83e88c6db2439be8b45239
Angel Siu <luisangelsiu2003@gmail.com>
2024-04-02 16:00:30 -0700

    main

--- fuente04.cbl
+++ fuente04.cbl
@@ -25,8 +25,58 @@
        01  ST-FILE     PIC XX.
        01  X           PIC X.
 
+       01  MENSAJE     PIC X(70).
+
+       01  FIN         PIC X       VALUES "N".
+
+       01  GUIONES     PIC X(80)   VALUES H"C4".
+
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
-            DISPLAY "Hello world"
-            STOP RUN.
+           PERFORM INICIALIZACION.
+           PERFORM ABRO-ARCHIVO.
+           PERFORM PROCESO THRU F-PROCESO UNTIL FIN = "S".
+           PERFORM CIERRO-ARCHIVO.
+           GO TO FINALIZAR.
+
+
+       INICIALIZACION.
+           MOVE "N" TO FIN.
+
+       ABRO-ARCHIVO.
+           OPEN I-O CLIENTES.
+           IF ST-SIC > "07"
+              STRING "Error al abrir Clientes " ST-SIC DELIMITED BY SIZE
+                  INTO MENSAJE
+              DISPLAY  MENSAJE LINE 10 COL 20
+              MOVE "S" TO FIN.
+
+
+       CIERRO-ARCHIVO.
+           CLOSE CLIENTES.
+
+
+       FINALIZAR.
+           EXIT PROGRAM.
+
+
+       PROCESO.
+           PERFORM MUESTRO-PANTALLA.
+
+
+       F-PROCESO.
+           EXIT.
+
+       MUESTRO-PANTALLA.
+           DISPLAY " "               LINE 1  COL 1 ERASE EOS
+                   "A.B.M. Clientes" LINE 3  COL 32
+                   GUIONES.          LINE 4  COL 1
+                   "ID Cliente : "   LINE 10 COL 10
+                   "Nombre     : "   LINE 12 COL 10
+                   "Direccion  : "   LINE 14 COL 10
+                   "Cod.Postal : "   LINE 16 COL 10
+                   "Categoria  : "   LINE 18 COL 10
+                   GUIONES.          LINE 22 COL 1.
+
+
        END PROGRAM "EJEMPLO".
